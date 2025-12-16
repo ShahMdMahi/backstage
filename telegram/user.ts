a602178f82ea47fda58541930619257dd15b6d3a
Shah Md. Mahi <shahmdmahi24@gmail.com>
2025-12-16 21:56:54 +0600

    fix: streamline database connection logic and enhance logging in user commands

--- telegram/user.ts
+++ telegram/user.ts
@@ -1,30 +1,14 @@
 "use server";
 
-import "dotenv/config";
 import { sendVerificationEmail } from "@/actions/email";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@/lib/prisma/client";
+import { prisma } from "@/lib/prisma";
 import { User } from "@/lib/prisma/client";
-
-const adapter = new PrismaPg({
-  connectionString: process.env.POSTGRES_PRISMA_URL!,
-});
-const prisma = new PrismaClient({
-  adapter,
-  log:
-    process.env.NODE_ENV === "development"
-      ? ["query", "error", "warn"]
-      : ["error"],
-});
 
 export async function getUserAllUsersForBot(): Promise<{
   success: boolean;
   users: Partial<User>[] | [];
 }> {
   try {
-    console.log("Connecting to database to fetch all users for bot");
-    await prisma.$connect();
-    console.log("Fetching all users for bot");
     const users = await prisma.user.findMany({
       select: {
         id: true,
@@ -40,18 +24,13 @@ export async function getUserAllUsersForBot(): Promise<{
     });
 
     if (users.length < 0) {
-      console.log("No users found for bot");
       return { success: false, users: [] };
     }
 
-    console.log("Successfully fetched all users for bot");
     return { success: true, users };
   } catch (error) {
     console.error("Error fetching users for bot:", error);
     return { success: false, users: [] };
-  } finally {
-    console.log("Disconnecting from database after fetching users for bot");
-    await prisma.$disconnect();
   }
 }
 
@@ -60,9 +39,6 @@ export async function getAllUnverifiedUsersForBot(): Promise<{
   users: Partial<User>[] | [];
 }> {
   try {
-    console.log("Connecting to database to fetch all unverified users for bot");
-    await prisma.$connect();
-    console.log("Fetching all unverified users for bot");
     const users = await prisma.user.findMany({
       where: { verifiedAt: null },
       select: {
@@ -79,20 +55,13 @@ export async function getAllUnverifiedUsersForBot(): Promise<{
     });
 
     if (users.length < 0) {
-      console.log("No unverified users found for bot");
       return { success: false, users: [] };
     }
 
-    console.log("Successfully fetched all unverified users for bot");
     return { success: true, users };
   } catch (error) {
     console.error("Error fetching unverified users for bot:", error);
     return { success: false, users: [] };
-  } finally {
-    console.log(
-      "Disconnecting from database after fetching unverified users for bot"
-    );
-    await prisma.$disconnect();
   }
 }
 
@@ -101,9 +70,6 @@ export async function getAllUnapprovedUsersForBot(): Promise<{
   users: Partial<User>[] | [];
 }> {
   try {
-    console.log("Connecting to database to fetch all unapproved users for bot");
-    await prisma.$connect();
-    console.log("Fetching all unapproved users for bot");
     const users = await prisma.user.findMany({
       where: { approvedAt: null },
       select: {
@@ -120,20 +86,13 @@ export async function getAllUnapprovedUsersForBot(): Promise<{
     });
 
     if (users.length < 0) {
-      console.log("No unapproved users found for bot");
       return { success: false, users: [] };
     }
 
-    console.log("Successfully fetched all unapproved users for bot");
     return { success: true, users };
   } catch (error) {
     console.error("Error fetching unapproved users for bot:", error);
     return { success: false, users: [] };
-  } finally {
-    console.log(
-      "Disconnecting from database after fetching unapproved users for bot"
-    );
-    await prisma.$disconnect();
   }
 }
 
@@ -142,9 +101,6 @@ export async function getAllSuspendedUsersForBot(): Promise<{
   users: Partial<User>[] | [];
 }> {
   try {
-    console.log("Connecting to database to fetch all suspended users for bot");
-    await prisma.$connect();
-    console.log("Fetching all suspended users for bot");
     const users = await prisma.user.findMany({
       where: { suspendedAt: { not: null } },
       select: {
@@ -161,20 +117,13 @@ export async function getAllSuspendedUsersForBot(): Promise<{
     });
 
     if (users.length < 0) {
-      console.log("No suspended users found for bot");
       return { success: false, users: [] };
     }
 
-    console.log("Successfully fetched all suspended users for bot");
     return { success: true, users };
   } catch (error) {
     console.error("Error fetching suspended users for bot:", error);
     return { success: false, users: [] };
-  } finally {
-    console.log(
-      "Disconnecting from database after fetching suspended users for bot"
-    );
-    await prisma.$disconnect();
   }
 }
 
@@ -183,8 +132,7 @@ export async function getUserByIdForBot(userId: string): Promise<{
   user: Partial<User> | null;
 }> {
   try {
-    console.log("Connecting to database to fetch user by ID for bot");
-    await prisma.$connect();
+    console.log("Fetching user by ID for bot:", userId);
     const user = await prisma.user.findUnique({
       where: { id: userId },
       select: {
@@ -201,20 +149,15 @@ export async function getUserByIdForBot(userId: string): Promise<{
     });
 
     if (!user) {
-      console.log("No user found by ID for bot");
+      console.log("User not found by ID for bot:", userId);
       return { success: false, user: null };
     }
 
-    console.log("Successfully fetched user by ID for bot");
+    console.log("User found by ID for bot:", user);
     return { success: true, user };
   } catch (error) {
     console.error("Error fetching user by ID for bot:", error);
     return { success: false, user: null };
-  } finally {
-    console.log(
-      "Disconnecting from database after fetching user by ID for bot"
-    );
-    await prisma.$disconnect();
   }
 }
 
@@ -223,9 +166,7 @@ export async function getUserByEmailForBot(email: string): Promise<{
   user: Partial<User> | null;
 }> {
   try {
-    console.log("Connecting to database to fetch user by email for bot");
-    await prisma.$connect();
-    console.log("Fetching user by email for bot");
+    console.log("Fetching user by email for bot:", email);
     const user = await prisma.user.findUnique({
       where: { email },
       select: {
@@ -242,20 +183,15 @@ export async function getUserByEmailForBot(email: string): Promise<{
     });
 
     if (!user) {
-      console.log("No user found by email for bot");
+      console.log("User not found by email for bot:", email);
       return { success: false, user: null };
     }
 
-    console.log("Successfully fetched user by email for bot");
+    console.log("User found by email for bot:", user);
     return { success: true, user };
   } catch (error) {
     console.error("Error fetching user by email for bot:", error);
     return { success: false, user: null };
-  } finally {
-    console.log(
-      "Disconnecting from database after fetching user by email for bot"
-    );
-    await prisma.$disconnect();
   }
 }
 
@@ -263,9 +199,6 @@ export async function approveUserByIdForBot(
   id: string
 ): Promise<{ success: boolean; user: Partial<User> | null }> {
   try {
-    console.log("Connecting to database to approve user by ID for bot");
-    await prisma.$connect();
-    console.log("Approving user by ID for bot");
     const user = await prisma.user.update({
       where: { id },
       data: { approvedAt: new Date() },
@@ -276,20 +209,13 @@ export async function approveUserByIdForBot(
     });
 
     if (!user.approvedAt) {
-      console.log("Failed to approve user by ID for bot");
       return { success: false, user: null };
     }
 
-    console.log("Successfully approved user by ID for bot");
     return { success: true, user };
   } catch (error) {
     console.error("Error approving user by ID for bot:", error);
     return { success: false, user: null };
-  } finally {
-    console.log(
-      "Disconnecting from database after approving user by ID for bot"
-    );
-    await prisma.$disconnect();
   }
 }
 
@@ -297,9 +223,6 @@ export async function unapproveUserByIdForBot(
   id: string
 ): Promise<{ success: boolean; user: Partial<User> | null }> {
   try {
-    console.log("Connecting to database to unapprove user by ID for bot");
-    await prisma.$connect();
-    console.log("Unapproving user by ID for bot");
     const user = await prisma.user.update({
       where: { id },
       data: { approvedAt: null },
@@ -310,20 +233,13 @@ export async function unapproveUserByIdForBot(
     });
 
     if (user.approvedAt) {
-      console.log("Failed to unapprove user by ID for bot");
       return { success: false, user: null };
     }
 
-    console.log("Successfully unapproved user by ID for bot");
     return { success: true, user };
   } catch (error) {
     console.error("Error unapproving user by ID for bot:", error);
     return { success: false, user: null };
-  } finally {
-    console.log(
-      "Disconnecting from database after unapproving user by ID for bot"
-    );
-    await prisma.$disconnect();
   }
 }
 
@@ -331,9 +247,6 @@ export async function suspendUserByIdForBot(
   id: string
 ): Promise<{ success: boolean; user: Partial<User> | null }> {
   try {
-    console.log("Connecting to database to suspend user by ID for bot");
-    await prisma.$connect();
-    console.log("Suspending user by ID for bot");
     const user = await prisma.user.update({
       where: { id },
       data: { suspendedAt: new Date() },
@@ -344,20 +257,13 @@ export async function suspendUserByIdForBot(
     });
 
     if (!user.suspendedAt) {
-      console.log("Failed to suspend user by ID for bot");
       return { success: false, user: null };
     }
 
-    console.log("Successfully suspended user by ID for bot");
     return { success: true, user };
   } catch (error) {
     console.error("Error suspending user by ID for bot:", error);
     return { success: false, user: null };
-  } finally {
-    console.log(
-      "Disconnecting from database after suspending user by ID for bot"
-    );
-    await prisma.$disconnect();
   }
 }
 
@@ -365,9 +271,6 @@ export async function unsuspendUserByIdForBot(
   id: string
 ): Promise<{ success: boolean; user: Partial<User> | null }> {
   try {
-    console.log("Connecting to database to unsuspend user by ID for bot");
-    await prisma.$connect();
-    console.log("Unsuspending user by ID for bot");
     const user = await prisma.user.update({
       where: { id },
       data: { suspendedAt: null },
@@ -378,20 +281,13 @@ export async function unsuspendUserByIdForBot(
     });
 
     if (user.suspendedAt) {
-      console.log("Failed to unsuspend user by ID for bot");
       return { success: false, user: null };
     }
 
-    console.log("Successfully unsuspended user by ID for bot");
     return { success: true, user };
   } catch (error) {
     console.error("Error unsuspending user by ID for bot:", error);
     return { success: false, user: null };
-  } finally {
-    console.log(
-      "Disconnecting from database after unsuspending user by ID for bot"
-    );
-    await prisma.$disconnect();
   }
 }
 
@@ -399,10 +295,6 @@ export async function resendVerificationEmailByIdForBot(
   id: string
 ): Promise<{ success: boolean; user: Partial<User> | null }> {
   try {
-    console.log(
-      "Connecting to database to resend verification email by ID for bot"
-    );
-    await prisma.$connect();
     const user = await prisma.user.findUnique({
       where: { id },
       select: {
@@ -414,31 +306,18 @@ export async function resendVerificationEmailByIdForBot(
     });
 
     if (!user) {
-      console.log(
-        "User not found for resending verification email by ID for bot"
-      );
       return { success: false, user: null };
     }
 
     if (user.verifiedAt) {
-      console.log(
-        "User already verified, no need to resend verification email for bot"
-      );
       return { success: false, user: null };
     }
 
-    console.log("Resending verification email for bot to:", user.email);
     await sendVerificationEmail(user.email);
 
-    console.log("Successfully resent verification email by ID for bot");
     return { success: true, user };
   } catch (error) {
     console.error("Error resending verification email by ID for bot:", error);
     return { success: false, user: null };
-  } finally {
-    console.log(
-      "Disconnecting from database after resending verification email by ID for bot"
-    );
-    await prisma.$disconnect();
   }
 }
